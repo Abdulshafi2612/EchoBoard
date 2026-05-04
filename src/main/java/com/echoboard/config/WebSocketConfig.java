5ffe17445dec591278617fb042a3b99e62ee3597
Mohammad Abdul-Shafi <mohamedsadik763@gmail.com>
2026-05-04 21:59:49 +0300

    rename pictures

--- src/main/java/com/echoboard/config/WebSocketConfig.java
+++ src/main/java/com/echoboard/config/WebSocketConfig.java
@@ -15,9 +15,6 @@ public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
-
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
     }
 
     @Override
